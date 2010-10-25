f4e161523af0e3bca456f53d242093d22519e415
Anatoly Sholomyansky <anatoly@commune2.com>
2010-10-25 19:13:33 -0400

    minor updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,11 @@ module ApplicationHelper
       x << link_to(category.name.upcase, categorized_path(category.name), :class => "menu") << tag("br")
     end
 
-    x << "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>"
+    x << "<br/><br/><br/><br/><br/><br/>"
 
-    x << link_to("home", root_path) << "<br/>"
-    x << link_to("about", about_path)
+    x << link_to("about / contact", about_path, :class => "menu") << "<br/><br/><br/><br/>"
+
+    x << link_to("home", root_path, :class => "menu")
     x
   end
 
